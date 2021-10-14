c717743ba57ddc403f7de94cccdbfb69c325aba1
ThanhManLab <Thanhmantm1999@gmail.com>
2021-10-14 10:46:35 +0700

    update 1

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 'use strict';
 const
   express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); 
+  bodyParser = require('body-parser');
+  const app = express();
+  app.use(bodyParser.json()); 
+
+  app.get('/',(req,res)=>{
+        return res.send('Hello World');
+    });
 app.listen(process.env.PORT || 8080, () => console.log('webhook is listening'));
