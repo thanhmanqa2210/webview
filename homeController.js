7e0af3fd03cf090b1e1a079e9c593d4ef8af52a0
ThanhManLab <Thanhmantm1999@gmail.com>
2021-11-20 17:47:00 +0700

    add 1

--- homeController.js
+++ homeController.js
@@ -1,6 +1,31 @@
+require('dotenv').config();
 let getHomePage=(req,res)=>{
     return res.send('Hello WOrld');
 }
 module.exports={
     getHomePage
+}
+let postWebhook=(req,res)=>{
+    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
 }
