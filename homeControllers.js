6615c22960cd34d632bf4d5975afeb8481eb7f60
ThanhManLab <Thanhmantm1999@gmail.com>
2021-11-20 17:20:05 +0700

    ttttt

--- homeControllers.js
+++ homeControllers.js
@@ -33,16 +33,16 @@ let postWebhook = (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-      Get the sender PSID
+    //   Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log("Sender PSID: " + sender_psid);
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handle.handleMessage(sender_psid, webhook_event.message);
-      } else if (webhook_event.postback) {
-       handle.handlePostback(sender_psid, webhook_event.postback);
-      }
+    //   if (webhook_event.message) {
+    //     handle.handleMessage(sender_psid, webhook_event.message);
+    //   } else if (webhook_event.postback) {
+    //    handle.handlePostback(sender_psid, webhook_event.postback);
+    //   }
     });
 
     // Returns a '200 OK' response to all requests
