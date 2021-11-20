bd6511838a9f484197511bab889842766f1b25da
ThanhManLab <Thanhmantm1999@gmail.com>
2021-11-21 00:34:54 +0700

    update 1

--- chatbotService/chatbotService.js
+++ chatbotService/chatbotService.js
@@ -30,36 +30,39 @@ function callSendAPI(sender_psid, response) {
 let getStartedButton = () => {
  
   let response = {
-    attachment: {
-      type: "template",
-      payload: {
-        template_type: "generic",
-        elements: [
-          {
-            title: "Welcome to Chatbot!",
-            image_url:
-              "https://lh3.googleusercontent.com/proxy/z5eiJPRzZekCHoyCqfu9_6Yo9EuaG083hN-cY-l7RHiDN8tO63gee5GsVIQDamTAESoUo6JS1EIOw3ivdnglXNKc1L_OtLbDFUaAtULTgNhoKUpIJs2l8ELG-edO5J7fAe97-Rk9bgukhpclHXFfWhCBosN5kBANe_ITilg",
-            subtitle:
-              "Online and mobile platform that connects local jobs or needs with freelancers quickly and conveniently, helping consumers find immediate help with daily tasks including cleaning. clean, move, repair and teach.",
-            default_action: {
-              type: "web_url",
-              url: `${WEBVIEW_URL}`,
-              messenger_extensions: true,
-              webview_height_ratio: "full",
-            },
-            buttons: [
-              {
-                type: "web_url",
-                url: `${WEBVIEW_URL}`,
-                title: "Start order",
-                messenger_extensions: false,
-                webview_height_ratio: "full",
+    "message":{
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "elements":[
+             {
+              "title":"Welcome!",
+              "image_url":"https://petersfancybrownhats.com/company_image.png",
+              "subtitle":"We have the right hat for everyone.",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://petersfancybrownhats.com/view?item=103",
+                "messenger_extensions": false,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://petersfancybrownhats.com/"
               },
-            ],
-          },
-        ],
-      },
-    },
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://petersfancybrownhats.com",
+                  "title":"View Website"
+                },{
+                  "type":"postback",
+                  "title":"Start Chatting",
+                  "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                }              
+              ]      
+            }
+          ]
+        }
+      }
+    }
   };
   return response;
 };
