97774cd9dc5a1e75002ec95cd54bde6068099516
ThanhManLab <Thanhmantm1999@gmail.com>
2021-11-21 03:10:29 +0700

    ek

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -6,7 +6,7 @@ const chatbotService = require("../chatbotService/chatbotService");
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 let getHomePage = (req, res) => {
   setupPersistentMenu();
-  return res.sendFile(path.join(__dirname+'html.html'));
+  return res.sendFile(path.join(__dirname+'/html.html'));
 
 };
 let postWebhook = (req, res) => {
